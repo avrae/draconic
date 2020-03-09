467fa8bd311a3e6ab3773c2d48053e3dc9593df7
Andrew Zhu <me@andrew-zhu.com>
2020-03-09 14:37:56 -0700

    add some test actions

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="draconic-interpreter",
+    version="0.0.1",
+    author="Andrew Zhu",
+    author_email="andrew@zhu.codes",
+    description="The interpreter for the Draconic scripting language.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/avrae/draconic",
+    packages=setuptools.find_packages(exclude=('tests',)),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
