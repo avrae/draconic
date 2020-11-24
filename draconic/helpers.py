425b046299fb2527a844bab82911fecb8d06dd97
Infinidoge <infinidoge@doge-inc.net>
2020-11-24 14:59:51 -0500

    Add compatibility check to support Python 3.8

--- draconic/helpers.py
+++ draconic/helpers.py
@@ -327,6 +327,9 @@ def safe_dict(config):
             self.__approx_len__ += other_lens
 
         def __or__(self, other):
+            if not hasattr(super(), "__or__"):
+                _raise_in_context(FeatureNotAvailable, "Dict Merge/Update is not yet available")
+
             if approx_len_of(self) + approx_len_of(other) > config.max_const_len:
                 _raise_in_context(IterableTooLong, "This dict is too large")
 
