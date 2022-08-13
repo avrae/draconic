dbe32ac5b63a0f31ed830e120ed13a4fc4cadb06
LazyDope <mydj555@gmail.com>
2022-08-13 16:49:14 -0400

    tests: move list comp tests

--- tests/test_limits.py
+++ tests/test_limits.py
@@ -70,6 +70,13 @@ def test_list(i, e):
     assert isinstance(e("reallist + my_list"), i._list)
     e("my_list.extend(reallist)")
     assert isinstance(e("my_list"), i._list)
+    
+    # check that you can't bypass cont limits with comprehension
+    i.builtins["range"] = range
+    e("[f'{x:03}' for x in range(250)]")
+    
+    with utils.raises(IterableTooLong):
+        e("[f'{x:03}' for x in range(251)]")
 
 
 def test_set(i, e):
@@ -157,12 +164,6 @@ def test_dict(i, e):
         
     with utils.raises(IterableTooLong):
         e("toolong2 = dict((f'{i:03}', 'a') for i in range(200, 343))")
-            
-def test_comprehension(i, e):
-    # check that you can't bypass cont limits with comprehension
-    with utils.raises(IterableTooLong):
-        i.builtins["range"] = range
-        e("[f'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa{x}' for x in range(1000)]")
 
 def test_that_it_still_works_right(i, e):
     e("l = [1, 2]")
